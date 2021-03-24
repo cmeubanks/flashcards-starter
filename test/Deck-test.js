889ebdd2f67e7c6107b22f9aa1878fd6397f39d2
Caroline Eubanks <cmeubanks@gmail.com>
2021-03-24 16:12:17 -0600

    completed deck class

--- test/Deck-test.js
+++ test/Deck-test.js
@@ -31,5 +31,18 @@ describe('Deck', function() {
 
     expect(cards).to.deep.equal([card1, card2, card3]);
     expect(deck.deck).to.equal(cards);
-  })
+  });
+
+  it('should count the number of cards in the deck', function() {
+    const card1 = new Card(1, "What allows you to define a set of related information using key-value pairs?", ["object", "array", "function"], "object")
+    const card2 = new Card(2, "What is a comma-separated list of related values?", ["array", "object", "function"], "array")
+    const card3 = new Card(3, "What type of prototype method directly modifies the existing array?", ["mutator method", "accessor method", "iteration method"], "mutator method")
+
+    const cards = [card1, card2, card3]
+    const deck = new Deck(cards);
+
+    const deckCount = deck.countCards();
+
+    expect(deckCount).to.equal(3);
+  });
 })
