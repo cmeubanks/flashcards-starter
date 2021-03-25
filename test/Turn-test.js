9a9c77572bb3347a9290fdecd7ce5a5c828f2594
Caroline Eubanks <cmeubanks@gmail.com>
2021-03-24 20:52:35 -0600

    fixed bug

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -17,45 +17,45 @@ describe('Turn', function() {
   });
 
   it('should store a guess', function() {
-    const currentCard = new Card();
-    const turn = new Turn('object', currentCard);
+    const card = new Card();
+    const turn = new Turn('object', card);
     expect(turn.guess).to.be.a('string');
     expect(turn.guess).to.equal('object');
   });
 
   it('should store the current card in play', function() {
-    const currentCard = new Card();
-    const turn = new Turn('object', currentCard);
-    expect(turn.currentCard).to.be.an('object');
-    expect(turn.currentCard).to.equal(currentCard);
+    const card = new Card();
+    const turn = new Turn('object', card);
+    expect(turn.card).to.be.an('object');
+    expect(turn.card).to.equal(card);
   });
 
   it('should return the user guess', function() {
-    const currentCard = new Card();
-    const turn = new Turn('object', currentCard);
+    const card = new Card();
+    const turn = new Turn('object', card);
     const guess = turn.returnGuess();
     expect(guess).to.be.a('string');
     expect(guess).to.equal('object');
   });
 
   it('should return the current card in play', function() {
-    const currentCard = new Card();
-    const turn = new Turn('object', currentCard);
-    const card = turn.returnCard();
-    expect(card).to.be.an('object');
-    expect(card).to.equal(currentCard);
+    const card = new Card();
+    const turn = new Turn('object', card);
+    const card1 = turn.returnCard();
+    expect(card1).to.be.an('object');
+    expect(card1).to.equal(card);
   });
 
   it('should determine if the guess matches the card correct answer', function() {
-    const currentCard = new Card();
-    const turn = new Turn('object', currentCard);
+    const card = new Card();
+    const turn = new Turn('object', card);
     const evaluation = turn.evaluateGuess();
     expect(evaluation).to.be.an('boolean');
   });
 
   it('should return a string response if the guess was incorrect', function() {
-    const currentCard = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'array');
-    const turn = new Turn('object', currentCard);
+    const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'array');
+    const turn = new Turn('object', card);
     const evaluation = turn.evaluateGuess();
     const feedback = turn.giveFeedback();
     expect(evaluation).to.equal(false);
@@ -63,8 +63,8 @@ describe('Turn', function() {
   });
 
   it('should return a string response if the guess was correct', function() {
-    const currentCard = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
-    const turn = new Turn('object', currentCard);
+    const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+    const turn = new Turn('object', card);
     const evaluation = turn.evaluateGuess();
     const feedback = turn.giveFeedback();
     expect(evaluation).to.equal(true);
