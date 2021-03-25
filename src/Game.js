be298434c62b7f847322518fd2912206e983b2f7
Caroline Eubanks <cmeubanks@gmail.com>
2021-03-25 08:50:59 -0600

    added creat card deck functionality

--- src/Game.js
+++ src/Game.js
@@ -1,10 +1,19 @@
 const data = require('./data');
 const prototypeQuestions = data.prototypeData;
 const util = require('./util');
+const Card = require('../src/Card.js');
 
 class Game {
   constructor() {
     this.currentRound = 0;
+  }
+
+  startGame() {
+    let cards = [];
+    prototypeData.forEach(element => {
+      const card = new Card(element.id, element.question, element.answers, element.correctAnswer);
+      cards.push(card);
+    })
   }
 
   printMessage(deck, round) {
