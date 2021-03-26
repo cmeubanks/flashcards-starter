d1dc9718d432dfcb6d5a329598ba986be5480233
Caroline Eubanks <cmeubanks@gmail.com>
2021-03-25 18:01:58 -0600

    completed round-test refactor

--- test/Round-test.js
+++ test/Round-test.js
@@ -6,14 +6,13 @@ const Deck = require('../src/Deck');
 const Round = require('../src/Round');
 
 describe ('Round', function() {
-  let card1, card2, card3, cards, deck, round
+  let card1, card2, card3, cards, deck, round;
 
   beforeEach(function() {
-    card1 = new Card(1, "What allows you to define a set of related information using key-value pairs?", ["object", "array", "function"], "object")
-    card2 = new Card(2, "What is a comma-separated list of related values?", ["array", "object", "function"], "array")
-    card3 = new Card(3, "What type of prototype method directly modifies the existing array?", ["mutator method", "accessor method", "iteration method"], "mutator method")
-
-    cards = [card1, card2, card3]
+    card1 = new Card(1, 'Which country produces the most coffee in the world?', ['Brazil', 'Columbia', 'Argentina'], 'Brazil');
+    card2 = new Card(2, "Which country invented tea?", ["England", "China", "India"], "China");
+    card3 = new Card(3, "What European nationa was said to invent hot dogs", ["Ireland", "Austria", "Germany"], "Germany");
+    cards = [card1, card2, card3];
     deck = new Deck(cards);
     round = new Round(deck);
   });
@@ -49,37 +48,36 @@ describe ('Round', function() {
   });
 
   it('should give tally the number of correct guesses after a guess is evaluated', function() {
-    const feedback = round.takeTurn("object");
+    const feedback = round.takeTurn("Brazil");
     expect(round.correctGuesses).to.equal(1);
     expect(feedback).to.equal('correct!');
   });
 
   it('should collect incorrect guesses if the guess is evaluated to false', function() {
-    const feedback = round.takeTurn("array");
+    const feedback = round.takeTurn("Columbia");
     expect(round.correctGuesses).to.equal(0);
     expect(round.incorrectGuesses.length).to.equal(1);
     expect(feedback).to.equal('incorrect!');
   });
 
   it('should update the current card for a new turn', function() {
-    round.takeTurn("object");
+    round.takeTurn("Brazil");
     expect(round.currentCard.id).to.equal(2);
   });
 
   it('should return the percent of correct guesses', function() {
-    round.takeTurn("object");
-    round.takeTurn("array");
+    round.takeTurn("Brazil");
+    round.takeTurn("China");
     let percent = round.calculatePercentCorrect();
     expect(percent).to.equal(100);
-    round.takeTurn("object")
+    round.takeTurn("Austria");
     percent = round.calculatePercentCorrect();
     expect(percent).to.equal(67);
-
   });
 
   it('returns a statement at the end of each round', function() {
-    round.takeTurn("object");
-    round.takeTurn("array");
+    round.takeTurn("Brazil");
+    round.takeTurn("China");
     const percent = round.calculatePercentCorrect();
     const endRound = round.endRound();
     expect(endRound).to.equal(`**Round over!You answered${percent}% of the questions correctly!`);
